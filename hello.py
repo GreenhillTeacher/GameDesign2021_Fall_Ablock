5f4caafe86a3fc361c4b3a42ecd4fb2af3b27cc0
suarezm <suarezm@greenhill.org>
2021-09-03 12:54:09 -0500

    This is my average commit

--- hello.py
+++ hello.py
@@ -1,5 +1,27 @@
+# This is for comments
 
-# By MAria Suarez
+# Maria Suarez
+# Date: 09/03/2021
+# This is our first program. test average
 
-# Welcome to my program
-print ("Hello")
+#Set variables
+test1=92
+test2=89
+test3=91
+test4=87
+
+#declare a average variable
+average=0
+print(type(average))
+#Four basic operators
+# Sum +
+# Sub -
+# Mult *
+# div /
+average= (test1+test2+test3+test4)/4
+# Let's print result
+print(type(average))
+#command  using keyword
+print("The tests average is ", average)
+
+
