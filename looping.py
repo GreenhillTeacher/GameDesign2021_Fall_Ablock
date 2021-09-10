ec97a8ff70c4b4a76d23dc2af982f5a0b2753854
Marisua <31741111+GreenhillTeacher@users.noreply.github.com>
2021-09-10 13:07:24 -0500

    Todays work

--- looping.py
+++ looping.py
@@ -1,16 +1,22 @@
 #Maria I Suarez
-# 
+# Libraries
 import os
 
 os.system('cls')
 star=int(input("How many stars? "))
 print("stars", star)
 line=star
+spc=0
 for i in range(line):
     #add a loop for the spaces
     for counter in range(star): #you can use a number or var
         print("*", end=" " )   #print on the same line
         #print(counter+1, end=" ")
+    for space in range (spc):
+        print(" ", end=" " )
+    for counter in range(star): #you can use a number or var
+        print("*", end=" " )   #print on the s
     print() #print a return creating a new line
     star-=1# short cut for star=star-1 
+    spc +=2
 print("Thank you! ")
---
+++ modulus.py
@@ -0,0 +1,19 @@
+#MAria Suarez
+#Learning about operators
+# + - / *  add, sub, div, multi
+# ** exp
+# % mosulus  reminder of a division
+#Find an even number
+
+num=int(input("Give me an integer"))
+print(num)
+if( num %2 == 0):
+    print(" the number is even")
+else:
+    print( "The number is odd")
+
+num2=float(input("give me any number no more than 2 decimals"))
+print(num2)
+print(num + num2)
+print(num2%2)
+
